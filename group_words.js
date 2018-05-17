b1e42d57b2543651c3e9c4a34484e0a7cc3e9224
MiklinMA <MiklinMA@gmail.com>
2018-05-17 09:50:20 +0500

    Group words

--- group_words.js
+++ group_words.js
@@ -1,3 +1,15 @@
+/*
+ * You have array of string. 
+ * You have to group the given words by having the same character 
+ * but in the different position
+ *
+ * Print something like:
+ * AMOR - ROMA - OMAR - MORA - RAMO - ARMO - MARO - ORAM
+ * XISELA - ALEXIS
+ * JAMON - MOJAN - MONJA
+ * ESPONJA - JAPONES
+ */
+
 words = ['AMOR', 'XISELA', 'JAMON', 'ROMA', 'OMAR', 'MORA', 'ESPONJA', 'RAMO', 'JAPONES', 'ARMO', 'MOJAN', 'MARO', 'ORAM', 'MONJA', 'ALEXIS']
 
 function group(words) {
@@ -13,5 +25,4 @@ function group(words) {
 }
 
 group(words)
-console.log()
 
